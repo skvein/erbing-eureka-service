2b0e4c1794930293635b83e81b4f6611178a62fa
skevin <584473028@qq.com>
2019-05-23 15:45:41 +0800

    eureka-service

---
+++ src/main/java/com/erbing/erbingeurekaservice/ErbingEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.erbing.erbingeurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ErbingEurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErbingEurekaServiceApplication.class, args);
+    }
+
+}
